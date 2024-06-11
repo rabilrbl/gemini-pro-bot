6fcabf28908490beb52eb2eb74f6c92701da2dac
uqe <arturkkos@gmail.com>
2024-06-11 10:39:53 +0300

    upd

--- gemini_pro_bot/handlers.py
+++ gemini_pro_bot/handlers.py
@@ -126,6 +126,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         await init_msg.edit_text("-")
         return
 
+    if update.message.from_user.id == 1625023138:
+        await init_msg.edit_text("-")
+        return
+
     # if update.message.from_user.id == 314254772:
     #     await init_msg.edit_text("пошёл нахуй, чертила ебаный")
     #     return
@@ -242,6 +246,10 @@ async def handle_image(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
     if update.message.from_user.id == 487532064:
+        await init_msg.edit_text("-")
+        return
+
+    if update.message.from_user.id == 1625023138:
         await init_msg.edit_text("-")
         return
 
